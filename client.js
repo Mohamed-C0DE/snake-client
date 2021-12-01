b7505e2de98e89762481c91128087a228f376740
Mohamed Ali <12maali27@gmail.com>
2021-12-01 22:05:17 +0000

    moved connect function from play.js to this file

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Your connection has been established");
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
