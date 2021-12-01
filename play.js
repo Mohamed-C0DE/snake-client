fcc51281454625219056204c683a778956cc01d6
Mohamed Ali <12maali27@gmail.com>
2021-12-01 23:58:25 +0000

    moving two functions to its own module and required one

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-// establishes a connection with the game server
 console.log("Connecting ...");
-let connection = connect();
+connect();
 
-// setup interface to handle user input from stdin
-const handleUserInput = function (data) {
-  if (data === "\u0003") {
-    console.log("exiting...");
-    process.exit();
-  }
-};
-
-const setupInput = function (connection) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-setupInput(connection);
+setupInput();
