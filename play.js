142e622ec201724ba2c2be58b0ec46344c8a5a13
Mohamed Ali <12maali27@gmail.com>
2021-12-01 23:47:48 +0000

    able to accept terminal input ctrl+c and exit the game

--- play.js
+++ play.js
@@ -2,4 +2,23 @@ const { connect } = require("./client");
 
 // establishes a connection with the game server
 console.log("Connecting ...");
-connect();
+let connection = connect();
+
+// setup interface to handle user input from stdin
+const handleUserInput = function (data) {
+  if (data === "\u0003") {
+    console.log("exiting...");
+    process.exit();
+  }
+};
+
+const setupInput = function (connection) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput(connection);
