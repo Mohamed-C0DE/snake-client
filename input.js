fcea40af64725d2b86797bf6dc1806f467620f98
Mohamed Ali <12maali27@gmail.com>
2021-12-02 00:23:54 +0000

    server able to recieve movement requests and apply them

--- input.js
+++ input.js
@@ -1,11 +1,27 @@
+let connection;
+console.log(connection);
+
 const handleUserInput = function (data) {
   if (data === "\u0003") {
     console.log("exiting...");
     process.exit();
   }
+  if (data === "w") {
+    connection.write("Move: up");
+  }
+  if (data === "a") {
+    connection.write("Move: left");
+  }
+  if (data === "s") {
+    connection.write("Move: down");
+  }
+  if (data === "d") {
+    connection.write("Move: right");
+  }
 };
 
-const setupInput = function (connection) {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
